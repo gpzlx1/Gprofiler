dafc8cb1b365cca598733fd76a021b8f41f40165
Your Name <you@example.com>
2021-04-14 22:38:41 +0800

    support multi-thread

--- Gprofiler.py
+++ Gprofiler.py
@@ -1,8 +1,12 @@
+import os
 import csv
 import json
 import time
+import pickle
+import threading
 from typing import Any
-
+from queue import Queue
+from multiprocessing import Manager
 
 global global_profiler
 global_profiler = None
@@ -12,40 +16,55 @@ def get_profiler():
     return global_profiler
 
 
-def set_profiler(output_file='Gprofiler', enable=True, basetime_now=True):
+def set_profiler(output_file=None, enable=True, basetime_now=True):
     global global_profiler
-    global_profiler = profiler("Gprofiler", enable=True, basetime_now=True)
+    if output_file is None:
+        output_file = str(os.getpid())
+    global_profiler = profiler(output_file, enable=enable, basetime_now=basetime_now)
+    return global_profiler
 
 
 class profiler(object):
     def __init__(self, output_name, enable=True, basetime_now=True):
         self.output_name = output_name
         self.enable = enable
+        self.queue = Queue()
         self.records = []
         self._processed = False
+        self._register_record_func = Manager().dict()
         if basetime_now:
             self.base = time.time()
 
     def __repr__(self):
         return "{}, {}, {}".format(self.output_name, self.enable, self.base)
 
-    def __record__(self, name, begin, end):
-        self.records.append((name, begin, end))
+    def __record__(self, record):
+        self.queue.put(record)
 
     def __process__(self):
         if self._processed:
             return 
-        new_records = []
+
+        while not self.queue.empty():
+            record = self.queue.get()
+            self.records.append(record)
+
         for item in self.records:
-            name = item[0]
-            begin = (item[1] - self.base) * 1e6
-            end = (item[2] - self.base) * 1e6
-            new_records.append((name, begin, end))
-        self.records = new_records
+
+            item[1] = (item[1] - self.base) * 1e6
+            item[2] = (item[2] - self.base) * 1e6
+
         self._processed = True
 
+    def __check_no_thread_running__(self):
+        if len(self._register_record_func) == 0:
+            return
+        for key, value in self._register_record_func.items():
+            if value == 'Running':
+                print("[Warning] tid {} still running".format(key))
 
     def save_as_csv(self):
+        self.__check_no_thread_running__()
         self.__process__()
         with open(self.output_name + '.csv', 'w') as csvfile:
             w = csv.writer(csvfile, delimiter=',')
@@ -53,6 +72,8 @@ class profiler(object):
             w.writerows(self.records)
             
     def save_as_tracing_json(self):
+        self.__check_no_thread_running__()
+        self.__process__()
         tracing_json = []
         for record in self.records:
             item = {}
@@ -60,7 +81,7 @@ class profiler(object):
             item["ph"] = 'X'
             item["ts"] = record[1]
             item["dur"] = record[2] - record[1]
-            item["tid"] = 1
+            item["tid"] = record[3]
             item["pid"] = self.output_name
             item["args"] = {}
             tracing_json.append(json.dumps(item))
@@ -72,7 +93,11 @@ class profiler(object):
             if len(tracing_json) > 0:
                 f.write("\t" + tracing_json[-1] + "\n")
             f.write(']')
-            
+
+    def save_as_gporfiler(self):
+        self.__check_no_thread_running__()
+        with open(self.output_name + ".gpout", "w") as f:
+            f.write(pickle.dumps(self))
 
 
 class record_function(object):
@@ -80,9 +105,15 @@ class record_function(object):
         self.name = name
         self.begin = None
         self.end = None
+        self.threadID = threading.current_thread().ident
+
+        global global_profiler
+        self.enable = global_profiler.enable and not global_profiler._processed
       
         
     def __enter__(self):
+        if not self.enable:
+            return 
         global global_profiler
 
         if global_profiler is None:
@@ -90,10 +121,14 @@ class record_function(object):
         if global_profiler.base is None:
             global_profiler.base = time.time()
 
+        global_profiler._register_record_func[self.threadID] = 'Running'
         self.begin = time.time()
         
         
     def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any):
+        if not self.enable:
+            return 
         global global_profiler
         self.end = time.time()
-        global_profiler.__record__(self.name, self.begin, self.end)
+        global_profiler._register_record_func[self.threadID] = 'Stop'
+        global_profiler.__record__([self.name, self.begin, self.end, self.threadID])
